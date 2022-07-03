81128ee0fd62d8fe552e977121c3f9d9fe529a18
Asteraye Tsigehymanot Molla <asteraye24@gmail.com>
2022-07-03 16:30:03 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,9 @@ include < stdio.h >
 
 /**
 * main - prints the size of various types
+*
+* Description: A C program that prints with printf function.
+*
 * Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)

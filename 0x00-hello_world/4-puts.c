e44254282c5306f31c750643e663f817bae363fe
Asteraye Tsigehymanot Molla <asteraye24@gmail.com>
2022-07-04 12:43:45 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * return: Always 0 (Success)
+ * main - entry point,
+ *
+ * Return: 0
  */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

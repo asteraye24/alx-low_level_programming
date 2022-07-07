bce4dbcda85f55f8a5584f720effbf3fff2e953e
Asteraye Tsigehymanot Molla <asteraye24@gmail.com>
2022-07-07 12:33:09 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - function to print abc 10 times
- *
- * Return: 0
  */
 void print_alphabet_x10(void)
 {
-	char c, i;
-	: wq
+	char c;
+	char i;
 
 		for (i = 0; i <= 9; i++)
 		{
@@ -15,6 +13,6 @@ void print_alphabet_x10(void)
 			{
 				_putchar(c);
 			}
-			_putchar('\n'); :
+			_putchar('\n');
 		}
 }

0e61b2e41c523a704cf17ee5938742c1e115f22e
Asteraye Tsigehymanot Molla <asteraye24@gmail.com>
2022-07-07 16:09:06 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ * followed by new line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	long int i, j, next;
+
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		} else
+		{
+			printf("ld\n", j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+
+	return (0);
+}

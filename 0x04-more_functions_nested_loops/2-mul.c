70a6b1a6100a1d06fa933787e27240523ab21daf
Asteraye Tsigehymanot Molla <asteraye24@gmail.com>
2022-07-09 16:43:08 -0700

    Task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * mul - multiplies two numbers
  * @a: integer param1
  * @b: integer param2
- * Return: success and integer
+ * Return: 0
  */
 
 int mul(int a, int b);

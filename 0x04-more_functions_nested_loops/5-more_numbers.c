536b0aa09dc0417bfad01b642a71e96c94e7a6dd
Asteraye Tsigehymanot Molla <asteraye24@gmail.com>
2022-07-10 12:29:32 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,9 @@ void more_numbers(void)
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 15; y++)
-			_putchar(x + '0');
+		{
+			_putchar(y + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

28918d25ff92693f844478b6e6b54b84bd3501c9
Asteraye Tsigehymanot Molla <asteraye24@gmail.com>
2022-07-10 16:14:53 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,7 @@ void fizz_buzz(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x == 100)
-			printf("Buzz ");
-		else if (x % 3 == 0)
+		if (x % 3 == 0)
 			printf("Fizz ");
 		else if (x % 5 == 0)
 			printf("Buzz ");

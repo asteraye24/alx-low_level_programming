846eca50444b646c56adf29a46454a3e9c38ad35
Asteraye Tsigehymanot Molla <asteraye24@gmail.com>
2022-07-12 14:05:55 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copy the string
+ * @dest: this is a destiny
+ * @src: the copy
+ *
+ * Return: this return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
+

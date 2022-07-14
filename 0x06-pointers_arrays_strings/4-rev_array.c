f4b319d7260a05a564507a12ef2fbac154a51a48
Asteraye Tsigehymanot Molla <asteraye24@gmail.com>
2022-07-14 16:39:40 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array -> reverses the content of array
+ * @a: an array
+ * @n: element of array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int *p, i, aux, k;
+
+	p = a;
+	for (i = 0; i < n; i++)
+		p++;
+	for (k = 0; k = n / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
+}

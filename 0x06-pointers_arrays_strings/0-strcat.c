244548dcd9c27d873a5cf715fb2e883896659698
Asteraye Tsigehymanot Molla <asteraye24@gmail.com>
2022-07-14 15:36:40 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strcat -> this a function strcat
@@ -9,12 +8,12 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int len + 0, i;
+	int len = 0, i;
 
 	while (dest[len])
 		len++;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; src[i] != '0'; i++)
 	{
 		dest[len] = src[i];
 		len += 1;
